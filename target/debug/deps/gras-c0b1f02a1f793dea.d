1baa59103298bfc8ae86473bb4228a42fb11bcff
saivishnu725 <saivishnu725@gmail.com>
2021-04-26 20:55:01 +0530

    Inital commit (Prints hello world)

---
+++ target/debug/deps/gras-c0b1f02a1f793dea.d
@@ -0,0 +1,5 @@
+/home/saivishnu/rust/gras/target/debug/deps/gras-c0b1f02a1f793dea: src/main.rs
+
+/home/saivishnu/rust/gras/target/debug/deps/gras-c0b1f02a1f793dea.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/gras.d
@@ -0,0 +1,1 @@
+/home/saivishnu/rust/gras/target/debug/gras: /home/saivishnu/rust/gras/src/main.rs
