359e2459f9fa9d3080a666859afdc5bc74e79346
saivishnu725 <saivishnu725@gmail.com>
2021-04-26 21:31:52 +0530

    Update .gitignore (Final edit hopefully)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-struct Cli {
-    pattern: String,
-    path: std::path::PathBuf,
-}
-
 fn main() {
     println!("Hello world!");
     let pattern = std::env::args().nth(1).expect("No pattern given!! ");
     let path = std::env::args().nth(2).expect("No path given!! ");
-    // println!("{}",pattern);
-    // println!("{}",path);
-    let _args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    println!("{}",pattern);
+    println!("{}",path);
 }
