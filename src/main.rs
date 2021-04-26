b154e20b3b0bf61e2246d01e010a573e8dd60b76
saivishnu725 <saivishnu725@gmail.com>
2021-04-26 21:23:21 +0530

    std::env::args method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello world!");
+    let pattern = std::env::args().nth(1).expect("No pattern given!! ");
+    let path = std::env::args().nth(2).expect("No path given!! ");
+    println!("{}",pattern);
+    println!("{}",path);
 }
