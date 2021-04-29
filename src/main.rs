692b5a5709f2c3c7e3340416de96a1d63d0c51dc
saivishnu725 <saivishnu725@gmail.com>
2021-04-29 16:45:31 +0530

    Used enums :)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use structopt::StructOpt;
 #[derive(StructOpt)]
-struct Cli{
+struct Cli {
     pattern: String,
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
@@ -9,17 +9,27 @@ struct Cli{
 fn main() {
     let args = Cli::from_args();
     let content = std::fs::read_to_string(&args.path).expect("File not found..");
-    println!("{}",content);
+    println!("{}", content);
     println!("`");
     println!("`");
     println!("`");
     println!("`");
     println!("`");
     println!("`");
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}",line);
-            println!("............");
+    // for line in content.lines() {
+    //     if line.contains(&args.pattern) {
+    //         println!("{}", line);
+    //         println!("............");
+    //     }
+    // }
+    println!("`");
+    println!("`");
+    let result = std::fs::read_to_string(&args.path);
+    let content = match result {
+        Ok(content) => content,
+        Err(error) => {
+            panic!("Cant deal with {} ,exiting here ", error);
         }
-    }
+    };
+    println!("{}", content);
 }
